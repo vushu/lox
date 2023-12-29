d32d04cc21579532752a06a7268cb46d412b2bfb
davu <davu@mir-robots.com>
2023-12-29 01:22:57 +0100

    adding test

--- tests/scanner_test.cpp
+++ tests/scanner_test.cpp
@@ -1,9 +1,15 @@
-#include<lox/scanner.hpp>
-#include<string_view>
+#include <lox/scanner.hpp>
+#include <string_view>
+#include <iostream>
 
 
 int main() {
-   std::string_view code = "2 + 2;";
+   std::string_view code = "2+2;";
    lox::Scanner scanner{code};
+   std::vector<lox::Token> tokens = scanner.scan_tokens();
+   for(lox::Token& tok : tokens) {
+        std::cout << tok.to_string() << std::endl;
+   }
+
    return 0;
 }
